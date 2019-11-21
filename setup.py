df2a082c6f61ab98461ce36eb9632f8e2b39e111
Frederic G. Østby <frederic.g.ostby@gmail.com>
2019-11-21 19:47:15 +0100

    Added Travis config

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
 
-from setuptools import setup, find_namespace_packages
+from setuptools import setup
 
 setup(
     name='okam-security',
     version='0.1.0',
+    python_requires='>=3.5',
     description='A Python port of the Mako (PHP) framework security library.',
     author='Frederic G. Østby',
     author_email='frederic.g.ostby@gmail.com',
+    packages=[
+        'okam.security',
+    ],
     project_urls={
         'Bug Tracker': 'https://github.com/freost/okam-security/issues',
         'Source Code': 'https://github.com/freost/okam-security',
@@ -17,6 +21,5 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
     ],
-    zip_safe=False,
-    packages=['okam.security']
+    zip_safe=False
 )
